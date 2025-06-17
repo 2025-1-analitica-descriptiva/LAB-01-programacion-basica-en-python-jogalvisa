7dc184a94ad454a11d3c39549f03e032391e8a1d
jogalvisa <jogalvisa@unal.edu.co>
2025-06-16 22:44:51 -0500

    .

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,7 +4,6 @@ datos requeridos se encuentran en el archivo data.csv. En este laboratorio
 solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
-# import csv
 
 def pregunta_01():
     """
@@ -14,17 +13,9 @@ def pregunta_01():
     214
 
     """
-    # with open("data.csv", "r") as file:
-    #     data = list(csv.reader(file, delimiter = "\t"))
     x =  open('D:\\Datos\\Documents\\CLASE ANALITICA DESCRIPTIVA\\LAB-01-programacion-basica-en-python-jogalvisa\\files\\input\\data.csv', 'r').readlines()
     x = [z.replace('\n','') for z in x]
     x = [z.split("\t") for z in x]
-    # data = []
-
-    # with open(input_file, "r", encoding="utf-8") as f:
-    #     reader = csv.DictReader(f)
-    #     for row in reader:
-    #         data.append(row)
     y = [z[1] for z in x[:]]
     numeros = [int(x) for x in y]
     suma = sum(numeros)
